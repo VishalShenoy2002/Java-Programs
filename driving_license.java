d6b7b1ea2d1106d21c97360ccc2b7fb43db17021
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2022-01-05 15:06:46 +0530

    Added Driving License Eligiblity program

---
+++ driving_license.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class driving_license {
+
+    public static void main(String[] args) {
+
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.println("Driving License Eligiblity.\n");
+
+        System.out.println("Enter Your Age:");
+        int age=scanner.nextInt();
+
+        if(age>=18 && age<=80){
+            System.out.println("You are Eligible to get your Driving License.");
+        }
+        else{
+            System.out.println("You are Not Eligible to get your Driving License.");
+        }
+
+        scanner.close();
+        
+    }
+    
+}
